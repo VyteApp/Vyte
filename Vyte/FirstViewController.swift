89a5a3c747e8de3b4fb697362e4e681c8bfcac3a
mpertierra <marcosp@mit.edu>
2015-03-24 02:02:15 -0400

    Initial Commit

---
+++ Vyte/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Vyte
+//
+//  Created by Marcos Alberto Pertierra Arrojo on 3/24/15.
+//  Copyright (c) 2015 Vyte. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Vyte/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Vyte
+//
+//  Created by Marcos Alberto Pertierra Arrojo on 3/24/15.
+//  Copyright (c) 2015 Vyte. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

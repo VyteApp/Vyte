eaed103c5312e192d65fbe8500fadd601585a7a9
Matthew Miklasevich <matthewmiklasevich@Balrog-Lord-of-Shadow-and-Flame.local>
2015-04-05 17:48:07 -0400

    Starting to integrate Parse

---
+++ Vyte/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  Vyte
+//
+//  Created by Matthew Miklasevich on 4/5/15.
+//  Copyright (c) 2015 Vyte. All rights reserved.
+//
+
+#ifndef Vyte_Bridging_Header_h
+#define Vyte_Bridging_Header_h
+
+#import <Parse/Parse.h>
+#import <ParseFacebookUtils/PFFacebookUtils.h>
+
+#endif

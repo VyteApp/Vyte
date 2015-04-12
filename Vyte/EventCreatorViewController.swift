92b8d9c6ebf8d2167437173a291d6f44060277df
Matthew Miklasevich <matthewmiklasevich@gmail.com>
2015-04-12 18:26:34 -0400

    PFGeoPoioPoint locations added

--- Vyte/EventCreatorViewController.swift
+++ Vyte/EventCreatorViewController.swift
@@ -7,16 +7,15 @@
 //
 
 import Foundation
+import MapKit
 
 class EventCreatorViewController: UIViewController {
     
     @IBOutlet weak var nameField: UITextField!
-    
     @IBOutlet weak var locationField: UITextField!
-    
     @IBOutlet weak var selectedDate: UILabel!
-    
     @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var doneButton: UIButton!
     
     @IBAction func datePickerAction(sender: AnyObject) {
         var dateFormatter = NSDateFormatter()
@@ -25,10 +24,38 @@ class EventCreatorViewController: UIViewController {
         self.selectedDate.text = date
     }
     
+    @IBAction func done(sender: UIButton) {
+        println("name: \(nameField.text)")
+        println("location: \(locationField.text)")
+        println("date: \(selectedDate.text)")
+        
+        CLGeocoder().geocodeAddressString(locationField.text, {(placemarks,error) -> Void in
+            if (error != nil) {println("error: \(error)")}
+            else if let placemark = placemarks?[0] as? CLPlacemark {
+                var placemark:CLPlacemark = placemarks[0] as CLPlacemark
+                var coordinates:CLLocationCoordinate2D = placemark.location.coordinate
+                println("latitude: \(coordinates.latitude)")
+                println("longitude: \(coordinates.longitude)")
+                var geopoint = PFGeoPoint(latitude: coordinates.latitude, longitude: coordinates.longitude)
+            }
+        })
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        nameField.resignFirstResponder()
+        locationField.resignFirstResponder()
+        return true;
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        nameField.resignFirstResponder()
+        locationField.resignFirstResponder()
+        self.view.endEditing(true)
     }
     
 }
